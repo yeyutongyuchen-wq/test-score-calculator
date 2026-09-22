0c3b554444547b356c205facdd3f6938578ffa51
ngunet <yeyutongyuchen@gmail.com>
2026-09-22 23:33:36 +0800

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,13 +3,13 @@ import { SITE_URL } from "@/lib/site";
 
 export const dynamic = "force-static";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: SITE_URL,
-      lastModified: "2026-09-22",
-      changeFrequency: "weekly",
-      priority: 1,
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
     },
-  ];
+    sitemap: `${SITE_URL}/sitemap.xml`,
+    host: SITE_URL,
+  };
 }
